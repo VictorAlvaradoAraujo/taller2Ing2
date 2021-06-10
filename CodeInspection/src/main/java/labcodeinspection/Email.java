2ab163f631c77e08545061f961b80e6a68e041f9
VictorAlvaradoAraujo <viosalva@espol.edu.ec>
2021-06-10 17:37:38 -0500

    Taller finalizado

--- CodeInspection/src/main/java/labcodeinspection/Email.java
+++ CodeInspection/src/main/java/labcodeinspection/Email.java
@@ -1,5 +1,12 @@
 package labcodeinspection;
 
+import java.util.logging.Logger;
+
+/**
+*
+*
+* @author Victor Alvarado
+*/
 public class Email {
 
 	private String m_firstName; //NOPMD  
@@ -9,17 +16,26 @@ public class Email {
 	private int defaultpasswordLength = 8; //NOPMD  
 	private String email;//NOPMD Correct
 
-	public Email(String firstName, String lastName) {
+	public Email(String firstName, String lastName) {//NOPMD contructor correct
 		this.m_firstName = firstName;
 		this.m_lastName = lastName;
 	}
-
+	/**
+	*
+	*
+	*  Function showInfo
+	*/
 	public void showInfo() {
-		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
-		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
-	}
 
-	public void setDeparment(int depChoice) {
+		Logger.getLogger("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
+		Logger.getLogger("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
+	}
+	/**
+	*
+	*
+	* @Function 
+	*/
+	public void setDeparment(final int depChoice) {
 		switch (depChoice) {
 		case 1:
 			this.department = "sales";
@@ -30,22 +46,29 @@ public class Email {
 		case 3:
 			this.department = "acct";
 			break;
+	    default: // empty case - this is interpreted as an intentional fall-through
+	    	this.department = "null";
+	    	break;
 		}
 	}
 
-	private String randomPassword(int length) {
-		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
-		char[] password = new char[length];
+	private String randomPassword(final int length) {
+		  String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*"; //NOPMD  
+		char[] password = new char[length];//NOPMD  
 		for (int i = 0; i < length; i++) {
-			int rand = (int) (Math.random() * set.length());
-			password[i] = set.charAt(rand);
+			final int rand = (int) (Math.random() * set.length());
+			password[i] = set.charAt(rand);//NOPMD  
 		}
 		return new String(password);
 	}
-
+	/**
+	*
+	*
+	* public method 
+	*/
 	public void generateEmail() {
 		this.password = this.randomPassword(this.defaultpasswordLength);
-		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase() + "@" + this.department
-				+ ".espol.edu.ec";
+		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase()//NOPMD is simple damage
+		+ "@" + this.department+ ".espol.edu.ec";
 	}
 }
--- CodeInspection/src/main/java/labcodeinspection/EmailApp.java
+++ CodeInspection/src/main/java/labcodeinspection/EmailApp.java
@@ -1,26 +1,53 @@
 package labcodeinspection;
 
 import java.util.Scanner;
-@SuppressWarnings("PMD.UseUtilityClass")
-public class EmailApp {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+/**
+ *  
+ * comments.
+ */
 
-		System.out.print("Enter your first name: ");
-		String firstName = sc.nextLine();
+public final class EmailApp { 
+	
+	  private EmailApp() {
+	        throw new UnsupportedOperationException();
+	    }
 
-		System.out.print("Enter your last name: ");
-		String lastName = sc.nextLine();
+	  /**
+	   * Metodo main 
+	   * 
+	   */
+	public static void main(final String args[] ) {
+		
+		final Scanner scLong = new Scanner(System.in); 
+		
+try {
+		System.out.print("Enter your first name: "); //NOPMD
+		final String firstName = scLong.nextLine();
 
-		System.out.print("\nDEPARTMENT CODE\n1. for sales\n2. for Development\n3. for accounting\nEnter code: ");
+		System.out.print("Enter your last name: ");//NOPMD
+		 final String lastName = scLong.nextLine();
 
-		int depChoice = sc.nextInt();
-		sc.close();
+		System.out.print("\nDEPARTMENT CODE\n1. for sales\n2. for Development\n3. for accounting\nEnter code: "); //NOPMD
 
-		Email email = new Email(firstName, lastName);
+		final int depChoice = scLong.nextInt();
+
+
+		
+
+		final Email email = new Email(firstName, lastName);
 		email.setDeparment(depChoice);
 		email.generateEmail();
 		email.showInfo();
+	
+}
+	finally {
+		
+		scLong.close(); 
 	}
+	
+		
+		
+	}
+	
 }
